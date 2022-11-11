a54ef85f478fd08a9eee95f7d8289735eb014459
Wender <wenderenzo2014@outlook.com>
2022-11-11 20:36:30 -0300

    Criando Back para usuarios

---
+++ src/app.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://wender:k8_-NNf_HbnC9bn@cluster0.kncufjy.mongodb.net/test'
+      ),
+      UsersModule
+    ],
+  controllers: [],
+  providers: [],
+})
+export class AppModule {}
