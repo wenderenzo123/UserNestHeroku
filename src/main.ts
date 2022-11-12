6ae4432468aa871b07669f6b16c9937bb5f468b9
Wender <wenderenzo2014@outlook.com>
2022-11-11 21:38:17 -0300

    Process Env port

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(
-    process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.get('env'));
+  });
 }
 bootstrap();
