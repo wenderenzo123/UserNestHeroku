de6ec7b66f36e4158747022eeede1c0159b305c1
Wender <wenderenzo2014@outlook.com>
2022-11-17 10:37:16 -0300

    Fix. Enviroments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UsersModule } from './users/users.module';
 import { MongooseModule } from '@nestjs/mongoose';
+require('dotenv').config()
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://wender:k8_-NNf_HbnC9bn@cluster0.kncufjy.mongodb.net/test'
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0.kncufjy.mongodb.net/${process.env.MONGO_DATABASE}`
       ),
       UsersModule
     ],
